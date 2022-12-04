6e86912eb42024bfb84f90649ca25d990ba5c6f7
Dejan <dejanvukovic1971@gmail.com>
2022-12-04 23:29:55 +0100

    promena

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set("view engine","ejs");
 app.get('/',(req,res)=>{
-    res.send("Ćao Mimi, kako si za mace?");
+    res.send("******Ćao Mimi, kako si za mace  ***?");
 }
 )
 
